844d45f25f366a7234fe8629a8e67a84ec0550a1
farhan-ahmad-tufail <contact.farhanahmad@gmail.com>
2024-11-03 19:23:26 +0530

    footer changes

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -180,7 +180,7 @@ const Footer = () => {
             })}
           </div>
 
-          <div className='text-center'>Made with ❤️ Parbhat Singh © 2024 Studynotion</div>
+          <div className='text-center'>Made with ❤️ Farhan Ahamd © 2024 StudyNotion</div>
         </div>
       </div>   
     </div>
